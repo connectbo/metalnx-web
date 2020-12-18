d35dd27dc780858dbfc16fb9460b5b46f04c4f76
Mike Conway <mike.conway@nih.gov>
2020-12-18 10:29:57 -0500

    #175 fixes for build

--- src/metalnx-web/src/main/java/com/emc/metalnx/controller/api/GalleryApiController.java
+++ src/metalnx-web/src/main/java/com/emc/metalnx/controller/api/GalleryApiController.java
@@ -10,11 +10,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.emc.metalnx.core.domain.exceptions.DataGridException;
 import com.emc.metalnx.services.interfaces.IRODSServices;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -45,6 +47,7 @@ public class GalleryApiController {
 		this.irodsServices = irodsServices;
 	}
 
+<<<<<<< HEAD
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public String list(@RequestParam("path") String path, @RequestParam("offset") int offset,
@@ -70,6 +73,19 @@ public class GalleryApiController {
 			throw new JargonException("unable to serialize", e);
 
 		}
+=======
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String list(final Model model, @RequestParam("path") String path, int offset, int limit)
+			throws DataGridException {
+
+		log.info("list()");
+
+		// List<GalleryEntry>() entries = galleryService.list(path, offset, limit);
+
+		// return ObjectMapper.write(entries);
+		return null;
+>>>>>>> #175 fixes for build
 	}
 
 }
